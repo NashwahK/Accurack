dc6f1cbfa2b0629a6d178e49f78eab1be34781c5
Nashwah <nashkaisar@gmail.com>
2025-07-12 20:18:31 +0300

    Final changes

--- accurack-web/src/app/departments/page.tsx
+++ accurack-web/src/app/departments/page.tsx
@@ -11,7 +11,7 @@ const DepartmentPage = () => {
         className="mb-6"
       />
       <h1 className="text-3xl font-bold mb-2">Sorry, this page is under construction</h1>
-      <p className="text-gray-500 text-lg">We're working hard to bring this feature to life. Stay tuned!</p>
+      <p className="text-gray-500 text-lg">{"We're working hard to bring this feature to life. Stay tuned!"}</p>
     </div>
   );
 };
